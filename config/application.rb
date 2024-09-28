8e378a4047b0a262f148bae30dd860c22a5818fe
rakesh9907 <rakeshsinghkushwah1998@gmail.com>
2024-09-28 02:32:27 +0000

    Added hosts in application file

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module StockPro
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.hosts << '3000-rakesh9907-protrader-fro-d3bzhf2f5v.app.codeanywhere.com'
   end
 end
